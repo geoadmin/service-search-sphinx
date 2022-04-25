4faac6d1b28526c287617b5161ffe7e5a3013f14
ltclm <marcel.clausen@swisstopo.ch>
2022-04-25 21:24:29 +0200

    BGDIINF_SB-2216: pg2spshinx endpoint will
    * not start local builds anymore
    * only use ecr images and always check for newer images on ecr.
    
    we have to assure to use the latest image of stable image names like
    develop-docker.latest or develop.latest, etc.

--- scripts/pg2sphinx.sh
+++ scripts/pg2sphinx.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
+docker_is_logged_in() {
+    jq -e --arg url "${DOCKER_REGISTRY}" '.auths | has($url)' ~/.docker/config.json > /dev/null
+}
+
+# check if we are already logged in
+if ! docker_is_logged_in; then
+    make dockerlogin
+fi
+
 if [ -n "${DB:-}" ]; then
     # call pg2sphinx trigger with DATABASE pattern
     ${DOCKER_EXEC} python3 pg2sphinx_trigger.py -s /etc/sphinxsearch/sphinx.conf -c update -d "${DB}"
