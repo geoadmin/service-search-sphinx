f39ca974b7f4424bfbb575faf2d89ea27773a5ac
Marcel Clausen <marcel.clausen@swisstopo.ch>
2022-09-01 09:01:33 +0200

    BGDIINF_SB-2530: minimal liveness probe

--- scripts/checker.sh
+++ scripts/checker.sh
@@ -3,29 +3,5 @@ set -e
 set -u
 set -o pipefail
 
-
-check_exit_code() {
-    exit_code=$?
-    # full path to output file
-    #local checker_file="/var/lib/container_probes/checker_ready.txt"
-
-    # analyze exit code with trapped function
-    if [ ${exit_code} -ne 0 ]; then
-        # Clear the readiness file
-        echo "ERROR: Liveness probe failed with code ${exit_code}" # | tee "${checker_file}"
-        exit_code=1
-    else
-        # TODO: implement robust readiness check
-        #./checker_ready.sh
-        echo "READY" # | tee ${checker_file}
-    fi
-    exit ${exit_code}
-}
-
-# Capture Exit Code with trapped pseudo signal EXIT
-# When the script is completed or exits for any reason, the
-# commands in the check_exit_code function will be executed.
-trap check_exit_code EXIT
-
-# Do the lifeness check
-searchd --status 1> /dev/null
+# Do the lifeness check exit status 0 -> success | 1 -> fail
+searchd --status 1> /dev/null && exit 0 || exit 1
