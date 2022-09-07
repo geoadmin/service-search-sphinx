20b24de7417253d0ba2ae167fc55c8e56cb08e2f
Marcel Clausen <marcel.clausen@swisstopo.ch>
2022-09-07 15:15:55 +0200

    feedback ltshb

--- scripts/checker.sh
+++ scripts/checker.sh
@@ -8,7 +8,7 @@ MOUNT=$(realpath "${PROBE_MOUNTPOINT:-/tmp}")
 READY_FILE="${MOUNT}/checker_ready.txt"
 
 clean_probe_files() {
-        rm -f "${READY_FILE}" || :
+    rm -f "${READY_FILE}" || :
 }
 
 # source this stuff until here
@@ -18,5 +18,6 @@ if searchd --status 1> /dev/null; then
     echo "READY" > "${READY_FILE}"
     exit 0
 else
+    clean_probe_files
     exit 1
 fi
