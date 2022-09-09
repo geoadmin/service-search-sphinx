9dc117a0d3726b6a4b7a853512f60277eb5bbd47
Marcel Clausen <marcel.clausen@swisstopo.ch>
2022-09-09 09:32:08 +0200

    use dedicated manticore indexes
    activate sync from efs

--- scripts/docker-cmd.sh
+++ scripts/docker-cmd.sh
@@ -37,10 +37,11 @@ done
 rm ${SPHINXINDEX_VOLUME}*.spl 2> /dev/null || :
 
 # sync new and updated indexes from efs to volume, ignore ongoing index creation (*.tmp.* files)
-#rsync --update --delete -av --exclude "*.tmp.*" --stats ${SPHINXINDEX_EFS} ${SPHINXINDEX_VOLUME} || :
+rsync --update --delete -av --exclude "*.tmp.*" --stats ${SPHINXINDEX_EFS} ${SPHINXINDEX_VOLUME} || :
 
 # start cron service as geodata user only if container is started in service mode (no cmd has been passed to docker run)
 # cron will sync every n minutes EFS to Docker volume and send a SIGHUP to searchd service
+# TODO: index-sync-rotate.sh has to be adapted first before activating the cronjob
 #service cron start || exit 1
 #crontab < docker-crontab
 
